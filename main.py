dc949536f7ee5baa6cc7718d18ab8a3e36e63a45
novarol <novarol3d@gmail.com>
2026-01-05 21:25:56 +0100

    feat: add asteroid field

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -17,10 +19,14 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
     Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    AsteroidField()
 
     while True:
         log_state()
