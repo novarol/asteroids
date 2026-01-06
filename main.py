704527c16419a002999ba29296bb5e58f82ae765
novarol <novarol3d@gmail.com>
2026-01-06 01:02:48 +0100

    feat: implement asteroid splitting

--- main.py
+++ main.py
@@ -48,6 +48,12 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.split()
+
         screen.fill("black")
 
         for obj in drawable:
